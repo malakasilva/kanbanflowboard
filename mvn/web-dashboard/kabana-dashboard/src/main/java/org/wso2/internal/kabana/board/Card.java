a5eab3b2735a9ab5d819b2ffc2d661071e93176f
malakasilva <malaka.silva@gmail.com>
2017-11-13 16:08:46 +0530

    Adding the web app

---
+++ mvn/web-dashboard/kabana-dashboard/src/main/java/org/wso2/internal/kabana/board/Card.java
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.internal.kabana.board;
+
+public class Card {
+
+    private String columnName; 
+    private String note;
+    private String title; 
+    private String projectName;
+    private String assigneeName;
+    private String assigneeProfile;
+    private String style;
+    
+
+    public Card(){
+    	
+    }
+    
+    public Card(boolean display){
+    	if(!display){
+    		style = "display:none";
+    	}
+    }
+    
+	public String getColumnName() {
+		return columnName;
+	}
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getAssigneeName() {
+		return assigneeName;
+	}
+	public void setAssigneeName(String assigneeName) {
+		this.assigneeName = assigneeName;
+	}
+	public String getAssigneeProfile() {
+		return assigneeProfile;
+	}
+	public void setAssigneeProfile(String assigneeProfile) {
+		this.assigneeProfile = assigneeProfile;
+	}
+  	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public String getDisplay(){
+		String rtnString = "";
+  		if(title != null && !title.trim().equals("")){
+  			rtnString = title;
+  		} else if (note != null) {
+  			rtnString = note;
+  		}
+  		if(rtnString.length() > 70) {
+  			rtnString = rtnString.substring(0, 65) + "...";	
+  		}
+  		return rtnString;
+  	}
+}
---
+++ mvn/web-dashboard/kabana-dashboard/src/main/java/org/wso2/internal/kabana/board/Row.java
@@ -0,0 +1,103 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.internal.kabana.board;
+
+public class Row {
+
+	private Card column1;
+	private Card column2;
+	private Card column3;
+	private Card column4;
+	private Card column5;
+	private Card column6;
+	private Card column7;
+	private Card column8;
+	private Card column9;
+	private Card column10;
+	private Card column11;
+	
+	public Card getColumn1() {
+		return column1;
+	}
+	public void setColumn1(Card column1) {
+		this.column1 = column1;
+	}
+	public Card getColumn2() {
+		return column2;
+	}
+	public void setColumn2(Card column2) {
+		this.column2 = column2;
+	}
+	public Card getColumn3() {
+		return column3;
+	}
+	public void setColumn3(Card column3) {
+		this.column3 = column3;
+	}
+	public Card getColumn4() {
+		return column4;
+	}
+	public void setColumn4(Card column4) {
+		this.column4 = column4;
+	}
+	public Card getColumn5() {
+		return column5;
+	}
+	public void setColumn5(Card column5) {
+		this.column5 = column5;
+	}
+	public Card getColumn6() {
+		return column6;
+	}
+	public void setColumn6(Card column6) {
+		this.column6 = column6;
+	}
+	public Card getColumn7() {
+		return column7;
+	}
+	public void setColumn7(Card column7) {
+		this.column7 = column7;
+	}
+	public Card getColumn8() {
+		return column8;
+	}
+	public void setColumn8(Card column8) {
+		this.column8 = column8;
+	}
+	public Card getColumn9() {
+		return column9;
+	}
+	public void setColumn9(Card column9) {
+		this.column9 = column9;
+	}
+	public Card getColumn10() {
+		return column10;
+	}
+	public void setColumn10(Card column10) {
+		this.column10 = column10;
+	}
+	public Card getColumn11() {
+		return column11;
+	}
+	public void setColumn11(Card column11) {
+		this.column11 = column11;
+	}
+	
+	
+	
+}
